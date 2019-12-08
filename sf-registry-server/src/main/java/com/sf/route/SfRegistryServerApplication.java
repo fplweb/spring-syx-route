ac858d35d1310439bf83c52ca1d0c6a84153a6b7
bini <193829947@qq.com>
2019-12-08 19:02:30 +0800

    init

---
+++ sf-registry-server/src/main/java/com/sf/route/SfRegistryServerApplication.java
@@ -0,0 +1,11 @@
+package com.sf.route;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfRegistryServerApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
