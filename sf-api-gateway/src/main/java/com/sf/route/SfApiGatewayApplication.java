a10e26411bd66fc2be13e3d58059f61ef9156070
bini <193829947@qq.com>
2019-12-08 19:55:41 +0800

    init

---
+++ sf-api-gateway/src/main/java/com/sf/route/SfApiGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.sf.route;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfApiGatewayApplication.class,args);
+    }
+}
